cb4d80f79b19d614f37abe38994645893e13ea27
Dah Taleb Abeidy <dahtalebabeidy@gmail.com>
2024-03-10 05:56:41 +0000

    Console

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Console module for the AirBnB clone project
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class for the command interpreter
+    """
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
