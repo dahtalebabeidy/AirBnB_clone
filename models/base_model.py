fdd9e870518233d917aecc62329bbba732143d2f
NezarMo7 <neln7aly110055@gmail.com>
2024-03-10 06:32:35 -0700

    b

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,8 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.created_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
