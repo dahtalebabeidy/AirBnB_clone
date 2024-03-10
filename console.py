08330707872865d328da173e20c1a8768f90fe29
NezarMo7 <116823232+NezarMo7@users.noreply.github.com>
2024-03-10 18:47:57 +0200

    Revert "unittest"
    This reverts commit 937b2b2f48f2ed0a4f9e36fd3fa8a09a659c7666.

--- console.py
+++ console.py
@@ -135,4 +135,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
---
+++ tests/test_save_reload_base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import os
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(project_root)
+
+from models import storage
+from models.base_model import BaseModel
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+my_model.save(storage)  # Pass storage as an argument
+print(my_model)
