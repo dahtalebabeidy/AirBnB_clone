c30c718522b22f3e09461d49ee2d8e9461a72886
Dah Taleb Abeidy <dahtalebabeidy@gmail.com>
2024-03-10 06:00:07 +0000

    Console

--- console.py
+++ console.py
@@ -19,5 +19,17 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program"""
         return True
 
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+    def help_quit(self):
+        """Print help for quit command"""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Print help for EOF command"""
+        print("EOF command to exit the program")
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
